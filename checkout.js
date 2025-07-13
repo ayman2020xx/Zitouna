29da98158cdf3380f0a8e62ffe62d0788bd8c27e
4yman <aitoufquirayman@gmail.com>
2025-07-13 16:54:17 +0100

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -81,3 +81,23 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+
+
+
+// Navbar toggle
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar && nav) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  });
+}
+
+if (close && nav) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+}
